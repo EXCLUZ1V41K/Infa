d000f5d282c2ceb62ca70d8dd621e46ffff7b48a
Александр <105676290+EXCLUZ1V41K@users.noreply.github.com>
2022-05-20 12:24:18 +0300

    Create 3.py

---
+++ Задание 1.2/Базовый уровень/3.py
@@ -0,0 +1,10 @@
+a = float(input("Введите число a: "))
+b = float(input("Введите число b: "))
+c = a + b
+c1 = a - b
+c2 = a * b
+c3 = a / b
+print("Сумма:", c)
+print("Разность:", c1)
+print("Произведение:", c2)
+print("Частное:", c3)
