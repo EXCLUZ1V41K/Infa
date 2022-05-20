89d2defc1d32d585ef28640d7ba56f4111786ca3
Александр <105676290+EXCLUZ1V41K@users.noreply.github.com>
2022-05-20 12:38:57 +0300

    Create 3.py

---
+++ Задание 2.1/Сложный уровень/3.py
@@ -0,0 +1,6 @@
+y = float(input("Введите ось Y: "))
+x = float(input("Введите ось X: "))
+if y == 2 and y >= 0 or x >= -1.4 and x <= 1.4:
+    print("Точка падает")
+else:
+    print("Точка не падает")
