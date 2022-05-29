3be44538dbbb452c7fff8440fd6d9d352d294634
Александр <105676290+EXCLUZ1V41K@users.noreply.github.com>
2022-05-29 16:47:43 +0300

    Update 3.py

--- Задание 1.1/Средний уровень/3.py
+++ Задание 1.1/Средний уровень/3.py
@@ -3,10 +3,9 @@ import math
 
 k = float(input("Введите число k: "))
 y = float(input("Введите число y: "))
-e = float(input("Введите число e: "))
 l = k - y
 if l <= 0:
    print("Решения нет!")
 else:
-   U = (math.log(l) + y ** 4) / (e ** y + 2.355 * k ** 2)
+   U = (math.log(l) + y ** 4) / (math.e ** y + 2.355 * k ** 2)
    print("Ответ:", U)
